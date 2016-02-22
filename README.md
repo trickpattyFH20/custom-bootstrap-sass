87294a8a933168426ad581bdbc0fb2e0d36e8427
Patrick Lawler <patricktlawler@gmail.com>
2016-02-22 17:49:11 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#automate custom bootstrap sass builds 
---
+++ buildcustom.js
@@ -0,0 +1,39 @@
+var fs = require('fs'),
+    clc = require('cli-color');
+    
+function customBsSass(configFile, bootstrapDir){
+    var configFile = configFile || [];
+    var basepath = bootstrapDir || 'bower_components/bootstrap-sass/assets/stylesheets/';
+    
+    var config = require('./'+configFile)
+
+    var sassfile = '_bootstrap.scss';
+
+    console.log('\n')
+    console.log(clc.underline.magenta('building custom bootstrap-sass...'))
+
+    try {
+        stats = fs.lstatSync(basepath + '_bootstrap-custom.scss');
+
+        if (stats.isFile()) {
+            console.log(clc.yellow('removing previous custom bootstrap sass build...'))
+            fs.unlink( basepath + '_bootstrap-custom.scss')
+        }
+    }
+    catch (e) {
+        console.log('new build')
+    }
+
+    fs.readFileSync(basepath + sassfile).toString().split('\n').forEach(function (line) { 
+        var newline = line.toString() + "\n";
+        for(var i=0,len=config.length;i<len;i++){
+            if(line.indexOf(config[i])!== -1){
+                newline = '//' + newline;
+            }
+        }
+        fs.appendFileSync(basepath + "_bootstrap-custom.scss", newline);
+    });
+
+    console.log(clc.green('success! \n'))
+}
+module.exports = customBsSass;
